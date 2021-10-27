a0fb3550fc32b7160aeb47ad62eb6a0a865c3d08
experiencei <ayelojahighbee01@gmail.com>
2021-10-27 14:31:39 +0100

    starting the cloning of google with next.js

--- components/header/Header.js
+++ components/header/Header.js
@@ -22,7 +22,9 @@ function Header() {
                     ref={searchInputRef}
                     type="text"
                 />
-                <XIcon className="h-7 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"/>
+                <XIcon className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
+                    onClick={() => (searchInputRef.current.value = "")}
+                />
             </form>
         </header>
     )
