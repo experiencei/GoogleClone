0f37a19f9b3ea67234674a6b19e57a2379ac587a
experiencei <ayelojahighbee01@gmail.com>
2021-10-27 14:47:58 +0100

    starting the cloning of google with next.js

--- components/header/Header.js
+++ components/header/Header.js
@@ -19,15 +19,16 @@ function Header() {
     }
 
     return (
-        <header>
-            <Image
+        <header className="sticky top-0 bg-white">
+        <div className="flex w-full p-6 items-center">
+        <Image
                 src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
                 width={120}
                 height={40}
                 onClick={() => router.push("/")}
                 className="cursor-pointer"
             />
-            <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+            <form className="flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
                 <input
                     className="flex-grow w-full focus:outline-none"
                     ref={searchInputRef}
@@ -45,6 +46,8 @@ function Header() {
                 />
                 <button hidden type="submit" onClick={search}>Search</button>
             </form>
+        </div>
+            
         </header>
     )
 }
