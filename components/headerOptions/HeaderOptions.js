7357e5e8467af862f91f10e4c902c91fc720717c
experiencei <ayelojahighbee01@gmail.com>
2021-10-27 15:24:47 +0100

    working on the avatar components

--- components/headerOptions/HeaderOptions.js
+++ components/headerOptions/HeaderOptions.js
@@ -10,7 +10,7 @@ import {
 
 
 function HeaderOptions() {
-    return  <div>
+    return  <div className="flex w-full text-gray-700 justify-evenly">
                 <div className="flex space-x-6">
                     <HeaderOption Icon={SearchIcon} title="All" selected/>
                     <HeaderOption Icon={PhotographIcon} title="Images" />
