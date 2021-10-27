96232e6a994ed0d1ab122714224ef5d813b2f4b5
experiencei <ayelojahighbee01@gmail.com>
2021-10-27 13:41:25 +0100

    starting the cloning of google with next.js

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -2,15 +2,26 @@ import { GlobeIcon } from "@heroicons/react/solid"
 
 function Footer() {
     return (
-        <footer className="grid w-full">
+        <footer className="grid w-full divide-y-[1px] divide-gray-300 bg-gray-100 text-sm text-gray-500">
         <div className="px-8 py-3" >
-            <p>Spain</p>
+            <p>Nigeria</p>
         </div>
 
-           <div>
-                 <div>
-                     
+           <div className="grid grid-cols-1 ">
+                 <div className="flex justify-center items-center">
+                    <GlobeIcon className="h-5 mr-1 text-green-700"/> Carbon neutral since 2007
                  </div>
+
+                  <div className="flex justify-center space-x-8 whitespace-nowrap md:justify-self-start">
+                      <p>Advertising</p>
+                      <p>Business</p>
+                      <p>How Search works</p>
+                  </div>
+                   <div className="flex justify-center space-x-8 md:ml-auto">
+                       <p>Privacy</p>
+                       <p>Terms</p>
+                       <p>Settings</p>
+                   </div>
            </div>
         </footer>
     )
