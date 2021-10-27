6dfbc0669cd1adcee8f05deeddfe4d351ba869a2
experiencei <ayelojahighbee01@gmail.com>
2021-10-27 14:02:36 +0100

    starting the cloning of google with next.js

---
+++ pages/search.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+function Search() {
+    return (
+        <Head>
+            <title>Search Results</title>
+            <link rel="icon" href="/favicon.ico" />
+
+            <Header/>
+        </Head>
+    )
+}
+
+export default Search
